f1e0fa589a22d3786bc6cff9889764f1bd250a70
Estefania Flores <geekwise.estefania.flores@gmail.com>
2019-12-23 14:13:04 -0800

    update: routed serializers t the models

--- app/drf/serializers.py
+++ app/drf/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from .models import Branch, Account, Customer
 
 class user_serializer( serializers.HyperlinkedModelSerializer ):
     class Meta:
