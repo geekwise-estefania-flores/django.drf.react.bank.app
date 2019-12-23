12b9b88f8688ac6e82be7e413399e885c8010ff4
Estefania Flores <geekwise.estefania.flores@gmail.com>
2019-12-23 14:04:31 -0800

    updated: added bank models

--- app/drf/models.py
+++ app/drf/models.py
@@ -2,10 +2,32 @@ from django.db import models
     # Create your models here.
 
 
-class Todo(models.Model):
-    title = models.CharField(max_length=120)
-    description = models.TextField()
-    completed = models.BooleanField(default=False)
+    class Branch(models.Model):
+        name = models.CharField(max_length=100)
+        address = models.CharField(max_length=200, default='123 Fake Street, 93291')
 
-    def _str_(self):
-        return self.title
+     def __str__(self):
+        return f"{self.name}: {self.address}"
+
+    class Customer(models.Model):
+        name = models.CharField(max_length=100)
+        branch = models.ForeignKey(
+        Branch,
+        on_delete=models.CASCADE,
+        null=True,
+    )
+
+    def __str__(self):
+        return f"{self.name}: {self.branch}"
+
+class Account(models.Model):
+    name = models.CharField(max_length=30)
+    balance = models.DecimalField(max_digits=100, decimal_places=2, default=0)
+    holder = models.OneToOneField(
+        Customer,
+        on_delete=models.CASCADE,
+        null=True,
+    )
+
+    def __str__(self):
+        return f"{self.name}: {self.holder}"
