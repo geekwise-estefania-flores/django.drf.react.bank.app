67933321db107ec32faa10b7f94079cbf61d65d5
Estefania Flores <geekwise.estefania.flores@gmail.com>
2020-01-01 15:23:33 -0800

    update: showing bank models

--- src/App.js
+++ src/App.js
@@ -1,107 +1,269 @@
 import React, { Component } from "react";
-import Modal from "./components/branchModal";
-import Modal from "./components/accountModal";
+import ModalBranch from "./components/branchModal";
 import axios from "axios";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      // viewCompleted: false,
       branchItem: {
         name: "",
         address: "",
-        // completed: false
       },
       branchList: [],
 
+      customerItem: {
+        name: "",
+        branch: "",
+      },
+      customerList: [],
+
+      // productItem: {
+      //   product_options: "",
+      //   product_owner: ""
+      // },
+      // productList: [],
+
       accountItem: {
-      name: "",
-      balance: "",
-      holder: "",
-    },
-    accountList: [],
+        name: "",
+        balance: "",
+        holder: ""
+      },
+      accountList: [],
+
+      branchActive: true,
+      customerActive: false,
+      // productActive: false,
+      accountActive: false
+    };
   }
-};
   componentDidMount() {
     this.refreshList();
-  }
+  };
   refreshList = () => {
     axios
       .get("https://django-drf-react-bank-project.herokuapp.com/api/branches/")
       .then(res => this.setState({ branchList: res.data.results }))
       .catch(err => console.log(err));
+    axios
+      .get("https://django-drf-react-bank-project.herokuapp.com/api/customers/")
+      .then(res => this.setState({ customerList: res.data.results }))
+      .catch(err => console.log(err));
+    // axios
+    //   .get("https://backend-bank-yuki.herokuapp.com/bank/products/")
+    //   .then(res => this.setState({ productList: res.data.results }))
+    //   .catch(err => console.log(err));
+    axios
+      .get("https://django-drf-react-bank-project.herokuapp.com/api/accounts/")
+      .then(res => this.setState({ accountList: res.data.results }))
+      .catch(err => console.log(err));
   };
-  displayCompleted = status => {
+
+  displayBranch = status => {
     if (status) {
-      return this.setState({ viewCompleted: true });
+      return this.setState({ 
+        branchActive: true,
+        customerActive: false,
+        // productActive: false,
+        accountActive: false
+       });
     }
-    return this.setState({ viewCompleted: false });
+    return this.setState({ branchActive: false });
   };
+
+  displayCustomer = status => {
+    if (status) {
+      return this.setState({ 
+        branchActive: false,
+        customerActive: true,
+        // productActive: false,
+        accountActive: false
+       });
+    }
+    return this.setState({ customerActive: false });
+  };
+
+  // displayProduct = status => {
+  //   if (status) {
+  //     return this.setState({ 
+  //       branchActive: false,
+  //       customerActive: false,
+  //       // productActive: true,
+  //       accountActive: false
+  //      });
+  //   }
+  //   return this.setState({ productActive: false });
+  // };
+
+  displayAccount = status => {
+    if (status) {
+      return this.setState({ 
+        branchActive: false,
+        customerActive: false,
+        // productActive: false,
+        accountActive: true
+       });
+    }
+    return this.setState({ accountActive: false });
+  };
+
   renderTabList = () => {
     return (
       <div className="my-5 tab-list">
         <span
-          onClick={() => this.displayCompleted(false)}
-          className={this.state.viewCompleted ? "active" : ""}
-        >
-          Accounts
-        </span>
-        <span
-          onClick={() => this.displayCompleted(false)}
-          className={this.state.viewCompleted ? "" : "active"}
+          onClick={() => this.displayBranch(true)}
+          className={this.state.branchActive ? "active" : ""}
         >
           Branches
         </span>
         <span
-          onClick={() => this.displayCompleted(false)}
-          className={this.state.viewCompleted ? "active" : ""}
+          onClick={() => this.displayCustomer(true)}
+          className={this.state.customerActive ? "active" : ""}
         >
           Customers
         </span>
-        <span
-          onClick={() => this.displayCompleted(false)}
-          className={this.state.viewCompleted ? "active" : ""}
+        {/* <span
+          onClick={() => this.displayProduct(true)}
+          className={this.state.productActive ? "active" : ""}
         >
           Products
+        </span> */}
+        <span
+          onClick={() => this.displayAccount(true)}
+          className={this.state.accountActive ? "active" : ""}
+        >
+          Accounts
         </span>
       </div>
     );
   };
   renderItems = () => {
-    // const { viewCompleted } = this.state;
-    const newItems = this.state.branchList
-    console.log(newItems)
-    return newItems.map(item => (
-      <li
-        key={item.id}
-        className="list-group-item d-flex justify-content-between align-items-center"
-      >
-        <span
-          className={`todo-title mr-2 ${
-            this.state.viewCompleted ? "completed-todo" : ""
-          }`}
-          title={item.name}
+    let newItems;
+    if(this.state.branchActive) {
+      newItems = this.state.branchList
+      return newItems.map(item => (
+        <li
+          key={item.id}
+          className="list-group-item d-flex justify-content-between align-items-center"
         >
-          {item.name}
-        </span>
-        <span>
-          <button
-            onClick={() => this.editItem(item)}
-            className="btn btn-secondary mr-2"
+          <span
+            className={`todo-title mr-2`}
+            title={item.name}
           >
-            {" "}
-            Edit{" "}
-          </button>
-          <button
-            onClick={() => this.handleDelete(item)}
-            className="btn btn-danger"
+            {item.name}
+          </span>
+          <span>
+            <button
+              onClick={() => this.editItem(item)}
+              className="btn btn-secondary mr-2"
+            >
+              {" "}
+              Edit{" "}
+            </button>
+            <button
+              onClick={() => this.handleDelete(item)}
+              className="btn btn-danger"
+            >
+              Delete{" "}
+            </button>
+          </span>
+        </li>
+      ));
+    }
+    else if(this.state.customerActive) {
+      newItems = this.state.customerList
+      return newItems.map(item => (
+        <li
+          key={item.id}
+          className="list-group-item d-flex justify-content-between align-items-center"
+        >
+          <span
+            className={`todo-title mr-2`}
+            title={item.name}
           >
-            Delete{" "}
-          </button>
-        </span>
-      </li>
-    ));
+            {item.name}<br/>{item.holder}
+          </span>
+          <span>
+            <button
+              onClick={() => this.editItem(item)}
+              className="btn btn-secondary mr-2"
+            >
+              {" "}
+              Edit{" "}
+            </button>
+            <button
+              onClick={() => this.handleDelete(item)}
+              className="btn btn-danger"
+            >
+              Delete{" "}
+            </button>
+          </span>
+        </li>
+      ));
+    }
+    // else if(this.state.productActive) {
+    //   newItems = this.state.productList
+    //   return newItems.map(item => (
+    //     <li
+    //       key={item.id}
+    //       className="list-group-item d-flex justify-content-between align-items-center"
+    //     >
+    //       <span
+    //         className={`todo-title mr-2`}
+    //         title={item.product_options}
+    //       >
+    //         {item.product_options}<br/>{item.product_owner}
+    //       </span>
+    //       <span>
+    //         <button
+    //           onClick={() => this.editItem(item)}
+    //           className="btn btn-secondary mr-2"
+    //         >
+    //           {" "}
+    //           Edit{" "}
+    //         </button>
+    //         <button
+    //           onClick={() => this.handleDelete(item)}
+    //           className="btn btn-danger"
+    //         >
+    //           Delete{" "}
+    //         </button>
+    //       </span>
+    //     </li>
+    //   ));
+    // }
+    else {
+      newItems = this.state.accountList
+      return newItems.map(item => (
+        <li
+          key={item.id}
+          className="list-group-item d-flex justify-content-between align-items-center"
+        >
+          <span
+            className={`todo-title mr-2`}
+            title={item.holder}
+          >
+            {item.holder}<br/>Balance: ${item.balance}
+          </span>
+          <span>
+            <button
+              onClick={() => this.editItem(item)}
+              className="btn btn-secondary mr-2"
+            >
+              {" "}
+              Edit{" "}
+            </button>
+            <button
+              onClick={() => this.handleDelete(item)}
+              className="btn btn-danger"
+            >
+              Delete{" "}
+            </button>
+          </span>
+        </li>
+      ));
+    }
+    
   };
   toggle = () => {
     this.setState({ modal: !this.state.modal });
@@ -124,22 +286,22 @@ class App extends Component {
       .then(res => this.refreshList());
   };
   createItem = () => {
-    const item = { name: "", address: "123 Fake Street 93291"};
-    this.setState({ branchItem: item, modal: !this.state.modal });
+    const item = { bank_name: "", location: ""};
+    this.setState({ activeItem: item, modal: !this.state.modal });
   };
   editItem = item => {
-    this.setState({ branchItem: item, modal: !this.state.modal });
+    this.setState({ activeItem: item, modal: !this.state.modal });
   };
   render() {
     return (
       <main className="content">
-        <h1 className="text-white text-uppercase text-center my-4">Bank of Stef</h1>
+        <h1 className="text-white text-uppercase text-center my-4">Estefania's Bank App</h1>
         <div className="row ">
           <div className="col-md-6 col-sm-10 mx-auto p-0">
             <div className="card p-3">
               <div className="">
                 <button onClick={this.createItem} className="btn btn-primary">
-                  Add task
+                  Add item
                 </button>
               </div>
               {this.renderTabList()}
@@ -150,8 +312,8 @@ class App extends Component {
           </div>
         </div>
         {this.state.modal ? (
-          <Modal
-            branchItem={this.state.branchItem}
+          <ModalBranch
+            activeItem={this.state.branchItem}
             toggle={this.toggle}
             onSave={this.handleSubmit}
           />
