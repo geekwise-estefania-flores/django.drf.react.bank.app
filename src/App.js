7175a54f7f4b250b463fff5557d6b56d59085f3c
Estefania Flores <geekwise.estefania.flores@gmail.com>
2020-01-07 13:51:42 -0800

    update: commented out information for customer

--- src/App.js
+++ src/App.js
@@ -170,37 +170,37 @@ class App extends Component {
         </li>
       ));
     }
-    else if(this.state.customerActive) {
-      newItems = this.state.customerList
-      return newItems.map(item => (
-        <li
-          key={item.id}
-          className="list-group-item d-flex justify-content-between align-items-center"
-        >
-          <span
-            className={`todo-title mr-2`}
-            title={item.name}
-          >
-            {item.name}<br/>{item.holder}
-          </span>
-          <span>
-            <button
-              onClick={() => this.editItem(item)}
-              className="btn btn-secondary mr-2"
-            >
-              {" "}
-              Edit{" "}
-            </button>
-            <button
-              onClick={() => this.handleDelete(item)}
-              className="btn btn-danger"
-            >
-              Delete{" "}
-            </button>
-          </span>
-        </li>
-      ));
-    }
+    // else if(this.state.customerActive) {
+    //   newItems = this.state.customerList
+    //   return newItems.map(item => (
+    //     <li
+    //       key={item.id}
+    //       className="list-group-item d-flex justify-content-between align-items-center"
+    //     >
+    //       <span
+    //         className={`todo-title mr-2`}
+    //         title={item.name}
+    //       >
+    //         {item.name}<br/>{item.holder}
+    //       </span>
+    //       <span>
+    //         <button
+    //           onClick={() => this.editItem(item)}
+    //           className="btn btn-secondary mr-2"
+    //         >
+    //           {" "}
+    //           Edit{" "}
+    //         </button>
+    //         <button
+    //           onClick={() => this.handleDelete(item)}
+    //           className="btn btn-danger"
+    //         >
+    //           Delete{" "}
+    //         </button>
+    //       </span>
+    //     </li>
+    //   ));
+    // }
     // else if(this.state.productActive) {
     //   newItems = this.state.productList
     //   return newItems.map(item => (
@@ -291,7 +291,7 @@ class App extends Component {
     this.setState({ branchItem: item, modal: !this.state.modal });
   };
   editItem = item => {
-    this.setState({ activeItem: item, modal: !this.state.modal });
+    this.setState({ branchItem: item, modal: !this.state.modal });
   };
   render() {
     return (
