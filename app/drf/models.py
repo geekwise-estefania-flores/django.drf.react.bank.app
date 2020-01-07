3fdda3ba68d0eedad56c9b68caa35bba5aade206
Estefania Flores <geekwise.estefania.flores@gmail.com>
2020-01-07 11:13:58 -0800

    update: added a comment for incorrect password

--- app/drf/models.py
+++ app/drf/models.py
@@ -1,5 +1,19 @@
 from django.db import models
+from django.contrib.auth import authenticate, login
 
+
+from django.contrib.auth import authenticate, login
+
+def my_view(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        # Redirect to a success page.
+        ...
+    else:
+        'Incorrect login'
 
 
 class Branch(models.Model):
