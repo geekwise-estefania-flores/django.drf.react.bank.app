4a538e93394fa9ed0baa44c5bd286dc7df70063f
Estefania Flores <geekwise.estefania.flores@gmail.com>
2020-01-08 10:58:33 -0800

    update: authentication

--- app/main/urls.py
+++ app/main/urls.py
@@ -5,6 +5,7 @@ from drf import views as drf_views
 
 router = routers.DefaultRouter()
 
+router.register(r'users', drf_views.user_viewset, 'users')
 router.register(r'accounts', drf_views.Account_Viewset, 'account')
 router.register(r'branches', drf_views.Branch_Viewset, 'branches')
 router.register(r'customers', drf_views.Customer_Viewset, 'customers')
