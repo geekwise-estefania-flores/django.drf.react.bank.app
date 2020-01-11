b9e83b314ce6a248d9deda42b67b2f5d15bea4ab
Estefania Flores <geekwise.estefania.flores@gmail.com>
2020-01-10 16:04:36 -0800

    update: added log out

--- app/frontend/src/components/accounts/login.js
+++ app/frontend/src/components/accounts/login.js
@@ -3,17 +3,15 @@ import { Link, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { login } from "../../actions/auth";
-
 export class Login extends Component {
-    state = {
-        username: "", 
+  state = {
+        username: "",
         password: ""
     };
-    static propTypes = {
-      login: PropTypes.func.isRequired,
-      isAuthenticated: PropTypes.bool
-  };
-
+  static propTypes = {
+        login: PropTypes.func.isRequired,
+        isAuthenticated: PropTypes.bool
+    };
 onSubmit = e => {
         e.preventDefault();
         this.props.login(this.state.username, this.state.password);
@@ -22,10 +20,10 @@ onSubmit = e => {
         this.setState({ [e.target.name]: e.target.value });
     }
     render() {
-      if(this.props.isAuthenticated){
-        return <Redirect to="/"/>
-    }
-    const { username, password } = this.state;
+        if(this.props.isAuthenticated){
+            return <Redirect to="/"/>
+        }
+        const { username, password } = this.state;
         return (
             <div className="col-md-6 m-auto">
               <div className="card card-body mt-5">
@@ -64,6 +62,6 @@ onSubmit = e => {
     }
 }
 const mapStateToProps = state => ({
-  isAuthenticated: state.auth.isAuthenticated
+    isAuthenticated: state.auth.isAuthenticated
 });
 export default connect(mapStateToProps, { login })(Login);
